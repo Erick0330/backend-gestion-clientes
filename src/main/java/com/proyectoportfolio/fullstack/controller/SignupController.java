4e19e2e8dcda289654e15d0d937fc6fe41d37420
Erick0330 <abdielrl0330@gmail.com>
2024-12-28 12:29:29 -0500

    Arreglada response del signup

--- src/main/java/com/proyectoportfolio/fullstack/controller/SignupController.java
+++ src/main/java/com/proyectoportfolio/fullstack/controller/SignupController.java
@@ -1,17 +1,21 @@
 package com.proyectoportfolio.fullstack.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.proyectoportfolio.fullstack.DTO.SignupRequest;
 import com.proyectoportfolio.fullstack.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/signup")
+@CrossOrigin(origins = "http://localhost:4200")
 public class SignupController {
 
     private final AuthService authService;
@@ -21,11 +25,16 @@ public class SignupController {
     }
 
     @PostMapping
-    public ResponseEntity<String> signupCustomer(@RequestBody SignupRequest signupRequest){
+    public ResponseEntity<String> signupCustomer(@RequestBody SignupRequest signupRequest) throws JsonProcessingException {
         boolean isUserCreated = authService.createCustomer(signupRequest);
 
         if(!isUserCreated){
-            return ResponseEntity.status(HttpStatus.CREATED).body("User created succesufully");
+            Map<String, String> response = new HashMap<>();
+            response.put("message", "User created successfully");
+
+            ObjectMapper mapper = new ObjectMapper(); String jsonResponse = mapper.writeValueAsString(response);
+
+            return ResponseEntity.status(HttpStatus.CREATED).body(jsonResponse);
         }
         else
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Failed to create user");
