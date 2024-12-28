db2d5345be2879d35d4ec0a371445a51a677a2ac
Erick0330 <abdielrl0330@gmail.com>
2024-12-28 10:17:04 -0500

    Dando acceso a swagger

--- src/main/java/com/proyectoportfolio/fullstack/configuration/SecurityConfig.java
+++ src/main/java/com/proyectoportfolio/fullstack/configuration/SecurityConfig.java
@@ -35,6 +35,8 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth.
                         requestMatchers("/signup", "/login", "/api/customers").permitAll()
+                        .requestMatchers("/doc/**").permitAll()
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                        // .requestMatchers("/api/customers/**").authenticated()
                         .requestMatchers("/admin/**").hasAuthority("ADMIN").anyRequest().authenticated())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
